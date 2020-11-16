5ca40072136150727f79107d54e998c07d3b2148
SOUMYE KUMAR <37400483+SoumyeKumar@users.noreply.github.com>
2020-11-17 01:29:58 +0530

    Liferun upload

---
+++ Assets/Scripts/GameM.cs
@@ -0,0 +1,94 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class GameM : MonoBehaviour
+{
+    public GameObject gameOverCanvas;
+    public GameObject showPower1;
+    public GameObject showPower2;
+    public GameObject showPower3;
+    public GameObject showPower4;
+    public GameObject showPower5;
+    public GameObject showPower6;
+    public GameObject showPower7;
+    public GameObject showPower8;
+    void Start()
+    {
+        Time.timeScale = 1;
+    }
+
+
+    public void GameOver()
+    {
+        gameOverCanvas.SetActive(true);
+        Time.timeScale = 0;
+    }
+
+    public void ShowPower1()
+    {
+        showPower1.SetActive(true);
+        Time.timeScale = 0;
+    }
+    public void ShowPower2()
+    {
+        showPower2.SetActive(true);
+        Time.timeScale = 0;
+    }
+    public void ShowPower3()
+    {
+        showPower3.SetActive(true);
+        Time.timeScale = 0;
+    }
+    public void ShowPower4()
+    {
+        showPower4.SetActive(true);
+        Time.timeScale = 0;
+    }
+    public void ShowPower5()
+    {
+        showPower5.SetActive(true);
+        Time.timeScale = 0;
+    }
+    public void ShowPower6()
+    {
+        showPower6.SetActive(true);
+        Time.timeScale = 0;
+    }
+    public void ShowPower7()
+    {
+        showPower7.SetActive(true);
+        Time.timeScale = 0;
+    }
+    public void ShowPower8()
+    {
+        showPower8.SetActive(true);
+        Time.timeScale = 0;
+    }
+
+    public void Replay()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void BackToMainMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    public void BackToGame()
+    {
+            showPower1.SetActive(false);
+            showPower2.SetActive(false);
+            showPower3.SetActive(false);
+            showPower4.SetActive(false);
+            showPower5.SetActive(false);
+            showPower6.SetActive(false);
+            showPower7.SetActive(false);
+            showPower8.SetActive(false);
+        Time.timeScale = 1;
+               
+    }
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,121 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using System.Threading;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+
+public class PlayerMovement : MonoBehaviour
+{
+    public GameM gamem;
+    public float jump = 50f;
+    bool onGround = false;
+    public Rigidbody2D rb;
+    public GameObject Power1;
+    public int score = 0;
+    public Text scoreText, highScoreText;
+  
+
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();}
+
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+        score++;
+        scoreText.text = "SCORE: " + score.ToString();
+        if (score == 5)
+        {
+            gamem.ShowPower1();
+        }
+        else if(score == 10)
+        {
+            gamem.ShowPower2();
+        }
+        else if (score == 15)
+        {
+            gamem.ShowPower3();
+        }
+        else if (score == 20)
+        {
+            gamem.ShowPower4();
+        }
+        else if (score == 25)
+        {
+            gamem.ShowPower5();
+        }
+        else if (score == 30)
+        {
+            gamem.ShowPower6();
+        }
+        else if (score == 35)
+        {
+            gamem.ShowPower7();
+        }
+        else if (score == 40)
+        {
+            gamem.ShowPower8();
+        }
+
+    }
+
+
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && onGround)
+        {
+            rb.velocity = Vector2.up * jump;
+        }
+        
+    }
+
+
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.collider.tag == "Ground")
+        {
+            onGround = true;
+        }
+
+        if (collision.collider.tag == "brick")
+        {
+            gamem.GameOver();
+            
+        }
+        
+    }
+
+
+
+    private void OnCollisionStay2D(Collision2D collision)
+    {
+        if (collision.collider.tag == "Ground")
+        {
+            onGround = true;
+        }
+    }
+
+
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.collider.tag == "Ground")
+        {
+            onGround = false;
+        }
+    }
+
+   
+
+
+    
+
+
+
+
+}
